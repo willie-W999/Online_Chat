76893b9b79c2ec9238ad8dc2ea3ea2f247e9768f
willie-W999 <wang01064@gmail.com>
2024-12-03 15:13:02 +0800

    Add files via upload

---
+++ chat/src/main/java/com/willie/chat/chat/ChatMessage.java
@@ -0,0 +1,23 @@
+package com.willie.chat.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType type;
+
+    public enum MessageType{
+        CHAT, JOIN, LEAVE
+    }
+}
